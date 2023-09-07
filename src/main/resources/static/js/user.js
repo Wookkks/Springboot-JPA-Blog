79c7dfb5160d2768b7442615dace614ce53ba64d
Wookkks <discdao@gmail.com>
2023-09-07 20:36:21 +0900

    일반 로그인 구현

---
+++ src/main/resources/static/js/user.js
@@ -0,0 +1,67 @@
+let index = {
+
+    init: function () {
+        $("#btn-save").on("click", ()=>{ // function(){}, ()=>{} 코드를 줄이려는게 아니라 this를 바인딩하기 위해서!!
+            this.save();
+        });
+        $("#btn-login").on("click", ()=>{
+            this.login();
+        })
+    },
+
+    save: function () {
+        // alert("user의 save함수 호출됨");
+        let data = {
+            username: $("#username").val(),
+            password: $("#password").val(),
+            email: $("#email").val()
+        };
+
+        // console.log(data);
+
+        // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청!!
+        // ajax 호출 시 default가 비동기 호출
+        // 이 코드 밑에 다른 코드가 있어도 회원가입 중에 수행됨!!
+        // ajax가 통신을 성공하고 json을 리턴해주면 서버가 자동으로 자바 오브젝트로 변환해주네요?!
+        $.ajax({
+            // 회원가입 수행 효청
+            type: "POST",
+            url: "/blog/api/user",
+            data: JSON.stringify(data), // http body 데이터
+            contentType: "application/json; charset=utf-8", // body 데이터가 어떤 타입인지 (MIME)
+            dataType: "json" // 요청을 서버로 하면 응답이 왔을 때 기본적으로 모든 것이 문자열 (생긴게 json이라면) => javascript 오브젝트로 변경
+        }).done(function (resp){
+            // 정상일 때 수행
+            alert("회원가입이 완료되었습니다.")
+            // console.log(resp);
+            location.href = "/blog";
+        }).fail(function (error){
+            // 실패할 때 수행
+            alert(JSON.stringify(error));
+        });
+
+    },
+
+    login: function () {
+        let data = {
+            username: $("#username").val(),
+            password: $("#password").val(),
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "/blog/api/user/login",
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        }).done(function (resp){
+            alert("로그인이 완료되었습니다.")
+            location.href = "/blog";
+        }).fail(function (error){
+            alert(JSON.stringify(error));
+        });
+
+    }
+}
+
+index.init();
--- src/main/webapp/WEB-INF/views/user/joinForm.jsp
+++ src/main/webapp/WEB-INF/views/user/joinForm.jsp
@@ -1,22 +1,22 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<%@ include file="../layout/header.jsp"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ include file="../layout/header.jsp" %>
 
 <div class="container">
-    <form action="/action_page.php">
+    <form>
         <div class="form-group">
             <label for="username">Username :</label>
             <input type="text" class="form-control" placeholder="Enter username" id="username">
         </div>
         <div class="form-group">
+            <label for="password">Password:</label>
+            <input type="password" class="form-control" placeholder="Enter password" id="password">
+        </div>
+        <div class="form-group">
             <label for="email">Email:</label>
             <input type="email" class="form-control" placeholder="Enter Email" id="email">
         </div>
-        <div class="form-group">
-            <label for="pwd">Password:</label>
-            <input type="password" class="form-control" placeholder="Enter password" id="pwd">
-        </div>
-        <button type="submit" class="btn btn-primary">회원가입</button>
     </form>
+    <button id="btn-save" class="btn btn-primary">회원가입</button>
 </div>
-
-<%@ include file="../layout/footer.jsp"%>
+<script src="/blog/js/user.js"></script>
+<%@ include file="../layout/footer.jsp" %>
